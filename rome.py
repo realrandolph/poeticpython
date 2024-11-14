a4b63986f59489ac6b4a5b9faae006c023a5903c
Randolph <104107862+realrandolph@users.noreply.github.com>
2024-11-13 18:38:43 -0600

    Add files via upload

---
+++ rome.py
@@ -0,0 +1,20 @@
+window = [] 
+# Looking out at the gray
+
+flame = 0
+# A game set for Nero 
+
+for i in range(5):
+    # Let's see if it's live
+    
+    array.append(i)
+    # Blaze from the sky
+    
+    flame++
+    # Igniting a fuss
+
+# Let's see what did grow 
+print(window)  
+
+print(flame) 
+# We've lost the game
